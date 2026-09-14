1a72153f92ba31f9dc1fd9620ac2719876ff93d3
unknown <jhanpimentel00@gmail.com>
2026-09-14 13:28:54 -0400

    ejercicio 4 realizado

---
+++ ejercicio4/Program.cs
@@ -0,0 +1,28 @@
+﻿static class LogLine
+{
+    // Método Main temporal para que compile y te permita probar
+    public static void Main()
+    {
+        Console.WriteLine(Message("[ERROR]: Operacion invalida"));
+        Console.WriteLine(LogLevel("[ERROR]: Operacion invalida"));
+        Console.WriteLine(Reformat("[INFO]: Operacion completada"));
+    }
+    public static string Message(string logLine)
+    {
+       int colonIndex = logLine.IndexOf(':');
+       return logLine.Substring(colonIndex + 1).Trim();
+
+    }
+
+    public static string LogLevel(string logLine)
+    {
+       int startIndex = logLine.IndexOf('[');
+       int endIndex = logLine.IndexOf(']');
+       return logLine.Substring(startIndex + 1, endIndex-startIndex-1).ToLower();
+    }
+
+    public static string Reformat(string logLine)
+    {
+       return $"{Message(logLine)} ({LogLevel(logLine)})";
+    }
+}
